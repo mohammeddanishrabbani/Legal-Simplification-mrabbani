22141df0fef3f68032305e35c16c07667fb8657f
mohammeddanishrabbani <mohammeddanishrabbani@gmail.com>
2025-05-02 20:34:38 +0000

    added

---
+++ SFT/llama_3.2_3B_QLORA/script.sh
@@ -0,0 +1,4 @@
+python3 SFT/llama_3.2_3B_QLORA/eval.py --eval_type incontext --shots 0;
+python3 SFT/llama_3.2_3B_QLORA/eval.py --eval_type incontext --shots 1;
+python3 SFT/llama_3.2_3B_QLORA/eval.py --eval_type incontext --shots 2;
+python3 SFT/llama_3.2_3B_QLORA/eval.py --eval_type finetune --shots 0;
